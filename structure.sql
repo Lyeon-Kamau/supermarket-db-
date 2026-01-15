b52a838284fe8bfa2426cd46c8620d98e4ec2f4d
Lyeon-Kamau <lyeon.kamau@strathmore.edu>
2026-01-15 12:55:38 +0300

    lines removed

--- structure.sql
+++ structure.sql
@@ -1,33 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 08, 2026 at 05:29 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `supermarket-db-`
---
 CREATE DATABASE IF NOT EXISTS `supermarket-db-` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `supermarket-db-`;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `attendancelog`
---
 
 CREATE TABLE `attendancelog` (
   `AttendanceID` int(11) NOT NULL,
@@ -38,11 +18,7 @@ CREATE TABLE `attendancelog` (
   `Status` enum('Present','Absent','Late','Half Day') DEFAULT 'Present'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `categories`
---
 
 CREATE TABLE `categories` (
   `CategoryID` int(11) NOT NULL,
@@ -52,11 +28,7 @@ CREATE TABLE `categories` (
   `FloorSection` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `customer`
---
 
 CREATE TABLE `customer` (
   `CustomerID` int(11) NOT NULL,
@@ -72,11 +44,7 @@ CREATE TABLE `customer` (
   `Status` enum('Active','Inactive') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `employee`
---
 
 CREATE TABLE `employee` (
   `EmployeeID` int(11) NOT NULL,
@@ -90,11 +58,9 @@ CREATE TABLE `employee` (
   `Status` enum('Active','On Leave','Terminated') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
 
---
 -- Table structure for table `pricehistory`
---
+
 
 CREATE TABLE `pricehistory` (
   `PriceHistoryID` int(11) NOT NULL,
@@ -105,11 +71,7 @@ CREATE TABLE `pricehistory` (
   `ChangedBy` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `products`
---
 
 CREATE TABLE `products` (
   `ProductID` int(11) NOT NULL,
@@ -125,11 +87,7 @@ CREATE TABLE `products` (
   `Status` enum('Active','Discontinued','Out of Stock') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `promotions`
---
 
 CREATE TABLE `promotions` (
   `PromotionID` int(11) NOT NULL,
@@ -143,11 +101,7 @@ CREATE TABLE `promotions` (
   `Status` enum('Active','Expired','Cancelled') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `purchaseorderitems`
---
 
 CREATE TABLE `purchaseorderitems` (
   `POItemID` int(11) NOT NULL,
@@ -158,11 +112,7 @@ CREATE TABLE `purchaseorderitems` (
   `Subtotal` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `purchaseorders`
---
 
 CREATE TABLE `purchaseorders` (
   `PurchaseOrderID` int(11) NOT NULL,
@@ -175,11 +125,7 @@ CREATE TABLE `purchaseorders` (
   `Notes` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `sales`
---
 
 CREATE TABLE `sales` (
   `SaleID` int(11) NOT NULL,
@@ -194,11 +140,7 @@ CREATE TABLE `sales` (
   `ReceiptNumber` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `salesitems`
---
 
 CREATE TABLE `salesitems` (
   `SaleItemID` int(11) NOT NULL,
@@ -210,11 +152,7 @@ CREATE TABLE `salesitems` (
   `Subtotal` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `supplier`
---
 
 CREATE TABLE `supplier` (
   `SupplierID` int(11) NOT NULL,
@@ -229,236 +167,157 @@ CREATE TABLE `supplier` (
   `Status` enum('Active','Inactive') DEFAULT 'Active'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
-
---
 -- Indexes for table `attendancelog`
---
 ALTER TABLE `attendancelog`
   ADD PRIMARY KEY (`AttendanceID`),
   ADD KEY `EmployeeID` (`EmployeeID`);
 
---
 -- Indexes for table `categories`
---
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`CategoryID`),
   ADD UNIQUE KEY `CategoryName` (`CategoryName`),
   ADD KEY `ParentCategoryID` (`ParentCategoryID`);
 
---
 -- Indexes for table `customer`
---
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`CustomerID`),
   ADD UNIQUE KEY `Phone` (`Phone`);
 
---
 -- Indexes for table `employee`
---
 ALTER TABLE `employee`
   ADD PRIMARY KEY (`EmployeeID`),
   ADD UNIQUE KEY `Email` (`Email`);
 
---
 -- Indexes for table `pricehistory`
---
 ALTER TABLE `pricehistory`
   ADD PRIMARY KEY (`PriceHistoryID`),
   ADD KEY `ProductID` (`ProductID`),
   ADD KEY `ChangedBy` (`ChangedBy`);
 
---
 -- Indexes for table `products`
---
 ALTER TABLE `products`
   ADD PRIMARY KEY (`ProductID`),
   ADD KEY `CategoryID` (`CategoryID`),
   ADD KEY `SupplierID` (`SupplierID`);
 
---
 -- Indexes for table `promotions`
---
 ALTER TABLE `promotions`
   ADD PRIMARY KEY (`PromotionID`),
   ADD KEY `ProductID` (`ProductID`),
   ADD KEY `CategoryID` (`CategoryID`);
 
---
 -- Indexes for table `purchaseorderitems`
---
 ALTER TABLE `purchaseorderitems`
   ADD PRIMARY KEY (`POItemID`),
   ADD KEY `PurchaseOrderID` (`PurchaseOrderID`),
   ADD KEY `ProductID` (`ProductID`);
 
---
 -- Indexes for table `purchaseorders`
---
 ALTER TABLE `purchaseorders`
   ADD PRIMARY KEY (`PurchaseOrderID`),
   ADD KEY `SupplierID` (`SupplierID`);
 
---
 -- Indexes for table `sales`
---
 ALTER TABLE `sales`
   ADD PRIMARY KEY (`SaleID`),
   ADD UNIQUE KEY `ReceiptNumber` (`ReceiptNumber`),
   ADD KEY `CustomerID` (`CustomerID`),
   ADD KEY `EmployeeID` (`EmployeeID`);
 
---
 -- Indexes for table `salesitems`
---
 ALTER TABLE `salesitems`
   ADD PRIMARY KEY (`SaleItemID`),
   ADD KEY `SaleID` (`SaleID`),
   ADD KEY `ProductID` (`ProductID`);
 
---
 -- Indexes for table `supplier`
---
 ALTER TABLE `supplier`
   ADD PRIMARY KEY (`SupplierID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
 -- AUTO_INCREMENT for table `attendancelog`
---
 ALTER TABLE `attendancelog`
   MODIFY `AttendanceID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `categories`
---
 ALTER TABLE `categories`
   MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `customer`
---
 ALTER TABLE `customer`
   MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `employee`
---
 ALTER TABLE `employee`
   MODIFY `EmployeeID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `pricehistory`
---
 ALTER TABLE `pricehistory`
   MODIFY `PriceHistoryID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `products`
---
 ALTER TABLE `products`
   MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `promotions`
---
 ALTER TABLE `promotions`
   MODIFY `PromotionID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `purchaseorderitems`
---
 ALTER TABLE `purchaseorderitems`
   MODIFY `POItemID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `purchaseorders`
---
 ALTER TABLE `purchaseorders`
   MODIFY `PurchaseOrderID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `sales`
---
 ALTER TABLE `sales`
   MODIFY `SaleID` int(11) NOT NULL AUTO_INCREMENT;
 
---
 -- AUTO_INCREMENT for table `salesitems`
---
 ALTER TABLE `salesitems`
   MODIFY `SaleItemID` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
-
---
 -- Constraints for table `attendancelog`
---
 ALTER TABLE `attendancelog`
   ADD CONSTRAINT `attendancelog_ibfk_1` FOREIGN KEY (`EmployeeID`) REFERENCES `employee` (`EmployeeID`) ON DELETE CASCADE;
 
---
 -- Constraints for table `categories`
---
 ALTER TABLE `categories`
   ADD CONSTRAINT `categories_ibfk_1` FOREIGN KEY (`ParentCategoryID`) REFERENCES `categories` (`CategoryID`) ON DELETE SET NULL;
 
---
 -- Constraints for table `pricehistory`
---
 ALTER TABLE `pricehistory`
   ADD CONSTRAINT `pricehistory_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ON DELETE CASCADE,
   ADD CONSTRAINT `pricehistory_ibfk_2` FOREIGN KEY (`ChangedBy`) REFERENCES `employee` (`EmployeeID`) ON DELETE SET NULL;
 
---
 -- Constraints for table `products`
---
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`),
   ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`SupplierID`) REFERENCES `supplier` (`SupplierID`) ON DELETE SET NULL;
 
---
 -- Constraints for table `promotions`
---
 ALTER TABLE `promotions`
   ADD CONSTRAINT `promotions_ibfk_1` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ON DELETE CASCADE,
   ADD CONSTRAINT `promotions_ibfk_2` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`) ON DELETE CASCADE;
 
---
 -- Constraints for table `purchaseorderitems`
---
 ALTER TABLE `purchaseorderitems`
   ADD CONSTRAINT `purchaseorderitems_ibfk_1` FOREIGN KEY (`PurchaseOrderID`) REFERENCES `purchaseorders` (`PurchaseOrderID`) ON DELETE CASCADE,
   ADD CONSTRAINT `purchaseorderitems_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`);
 
---
 -- Constraints for table `purchaseorders`
---
 ALTER TABLE `purchaseorders`
   ADD CONSTRAINT `purchaseorders_ibfk_1` FOREIGN KEY (`SupplierID`) REFERENCES `supplier` (`SupplierID`);
 
---
 -- Constraints for table `sales`
---
 ALTER TABLE `sales`
   ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE SET NULL,
   ADD CONSTRAINT `sales_ibfk_2` FOREIGN KEY (`EmployeeID`) REFERENCES `employee` (`EmployeeID`);
 
---
 -- Constraints for table `salesitems`
---
 ALTER TABLE `salesitems`
   ADD CONSTRAINT `salesitems_ibfk_1` FOREIGN KEY (`SaleID`) REFERENCES `sales` (`SaleID`) ON DELETE CASCADE,
   ADD CONSTRAINT `salesitems_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
