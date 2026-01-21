d7f202d7a94398ae05fbb09b52bca22ee9295d9d
melvis1244 <mwenda.mwiti@strathmore.edu>
2026-01-21 12:44:28 +0300

    Views

--- structure.sql
+++ structure.sql
@@ -499,3 +499,97 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+--VIEWS--
+
+--Customer Purchase Summary--
+CREATE VIEW view_customer_sales_summary AS
+SELECT 
+    c.CustomerID,
+    CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName,
+    COUNT(s.SaleID) AS TotalSales,
+    SUM(s.TotalAmount) AS TotalSpent
+FROM customer c
+LEFT JOIN sales s ON c.CustomerID = s.CustomerID
+GROUP BY c.CustomerID;
+
+--Daily Sales Report--
+CREATE VIEW view_daily_sales AS
+SELECT 
+    DATE(SaleDate) AS SaleDay,
+    COUNT(SaleID) AS NumberOfSales,
+    SUM(SubTotal) AS SubTotal,
+    SUM(TaxAmount) AS TotalTax,
+    SUM(DiscountAmount) AS TotalDiscount,
+    SUM(TotalAmount) AS TotalRevenue
+FROM sales
+GROUP BY DATE(SaleDate);
+
+--Product Sales Performance--
+CREATE VIEW view_product_sales AS
+SELECT 
+    p.ProductID,
+    p.ProductName,
+    SUM(si.Quantity) AS TotalQuantitySold,
+    SUM(si.Subtotal) AS TotalSalesValue
+FROM products p
+JOIN salesitems si ON p.ProductID = si.ProductID
+GROUP BY p.ProductID;
+
+--Employee Sales Performance--
+CREATE VIEW view_employee_sales AS
+SELECT 
+    e.EmployeeID,
+    CONCAT(e.FirstName, ' ', e.LastName) AS EmployeeName,
+    COUNT(s.SaleID) AS SalesHandled,
+    SUM(s.TotalAmount) AS TotalSalesValue
+FROM employee e
+LEFT JOIN sales s ON e.EmployeeID = s.EmployeeID
+GROUP BY e.EmployeeID;
+
+--Active Promotions--
+CREATE VIEW view_active_promotions AS
+SELECT 
+    PromotionID,
+    PromotionName,
+    DiscountPercentage,
+    StartDate,
+    EndDate,
+    Status
+FROM promotions
+WHERE Status = 'Active';
+
+--Product Price List--
+CREATE VIEW view_product_price_list AS
+SELECT 
+    ProductID,
+    ProductName,
+    SellingPrice,
+    Status
+FROM products
+WHERE Status = 'Active';
+
+--Supplier Product List--
+CREATE VIEW view_supplier_products AS
+SELECT 
+    s.SupplierName,
+    p.ProductName,
+    p.CostPrice,
+    p.SellingPrice
+FROM supplier s
+JOIN products p ON s.SupplierID = p.SupplierID;
+
+--Employee Attendance Summary--
+CREATE VIEW view_employee_attendance AS
+SELECT 
+    e.EmployeeID,
+    CONCAT(e.FirstName, ' ', e.LastName) AS EmployeeName,
+    COUNT(a.AttendanceID) AS DaysRecorded,
+    SUM(a.Status = 'Present') AS DaysPresent,
+    SUM(a.Status = 'Absent') AS DaysAbsent
+FROM employee e
+LEFT JOIN attendancelog a ON e.EmployeeID = a.EmployeeID
+GROUP BY e.EmployeeID;
+
+
+
